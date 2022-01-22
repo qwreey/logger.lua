7a0c608334d5ffc1e209d8cf1200753fe104030c
세젤귀 고양이들 <46598063+qwreey75@users.noreply.github.com>
2022-01-22 14:24:00 +0900

    add lit package.lua

---
+++ package.lua
@@ -0,0 +1,11 @@
+return {
+	name = 'qwreey75/logger.lua',
+	version = '1.0.3',
+	homepage = 'https://github.com/qwreey75/logger.lua',
+	dependencies = {
+	},
+	tags = {'logger','logging'},
+	license = 'MIT',
+	author = 'qwreey75',
+	files = {'**.lua'},
+}
--- test.lua
+++
@@ -1,7 +0,0 @@
-_G.require = require -- must be need!
-local logger = require "logger"
-
-local user = {}
-user.name = "Qwreey"
-
-logger.infof("User Name Is : %s",user.name)
